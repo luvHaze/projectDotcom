a024cb8a8dc58178cd642ddb26458361a8c4e1ef
Zoey <53253298+luvHaze@users.noreply.github.com>
2020-05-21 22:21:49 +0900

    DAY1.2 로그인 화면 VideoView로 배경화면 동영상으로 함        (다른 동영상 목록 필요)
    DAY1.3 ViewPager로 메인메뉴 뼈대 설계
    
    DAY1.4 ViewPager로 메인메뉴 인디케이터, 앱바 생성

--- Dotcom/app/src/main/java/luv/zoey/dotcom/MenuActivity.kt
+++ Dotcom/app/src/main/java/luv/zoey/dotcom/MenuActivity.kt
@@ -2,7 +2,9 @@ package luv.zoey.dotcom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.FragmentPagerAdapter
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_menu.*
 import luv.zoey.dotcom.fragment.Fragment_CaseSharing
 import luv.zoey.dotcom.fragment.Fragment_Matching
@@ -11,18 +13,36 @@ import luv.zoey.dotcom.fragment.menuFragmentAdapter
 
 class MenuActivity : AppCompatActivity() {
 
+    val toolbar by lazy {
+        findViewById<Toolbar>(R.id.toolbar)
+    }
+    val fragmentAdapter by lazy {
+        menuFragmentAdapter(supportFragmentManager)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
+        init()
 
-        var fragmentAdapter = menuFragmentAdapter(supportFragmentManager)
-            .apply {
-            addItem(Fragment_CaseSharing())
+    }
+
+    private fun init(){
+        // [액션바] 초기화 단계
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+
+        // [페이지뷰] 초기화 단계
+        fragmentAdapter.apply {
             addItem(Fragment_Matching())
+            addItem(Fragment_CaseSharing())
             addItem(Fragment_RepairShop())
-            }
-        mainMenu_ViewPager.adapter=fragmentAdapter
+        }
 
+        mainMenu_ViewPager.adapter=fragmentAdapter
+        mainMenu_Indicator.setupWithViewPager(mainMenu_ViewPager,true)
     }
 }
