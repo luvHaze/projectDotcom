b7afa32e5a53184b0f8ac5bfec935ae562447895
Zoey <53253298+luvHaze@users.noreply.github.com>
2020-05-25 21:26:14 +0900

    DAY1.2 로그인 화면 VideoView로 배경화면 동영상으로 함        (다른 동영상 목록 필요)
    DAY1.3 ViewPager로 메인메뉴 뼈대 설계
    
    DAY1.4 메인메뉴 인디케이터, 앱바 생성
           페이지 구성 대부분 완료
           * TODO (시작하기 버튼, 로그인창 Exoplayer 교체) - finish
    
    DAY1.5 * Exoplayer 교체 완료
           * 시작하기 버튼 생성 완료
    
    DAY1.6 * DrawerLayout추가 NavigationView추가 (기존 사용하던 앱바 설정 제거)
           * TODO ExoPlayer 영상 추가

--- Dotcom/app/src/main/java/luv/zoey/dotcom/LoginActivity.kt
+++ Dotcom/app/src/main/java/luv/zoey/dotcom/LoginActivity.kt
@@ -3,6 +3,7 @@ package luv.zoey.dotcom
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
--- Dotcom/app/src/main/java/luv/zoey/dotcom/MenuActivity.kt
+++ Dotcom/app/src/main/java/luv/zoey/dotcom/MenuActivity.kt
@@ -3,19 +3,23 @@ package luv.zoey.dotcom
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.FragmentPagerAdapter
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_menu.*
+import kotlinx.android.synthetic.main.toolbar_layout.*
 import luv.zoey.dotcom.fragment.Fragment_CaseSharing
 import luv.zoey.dotcom.fragment.Fragment_Matching
 import luv.zoey.dotcom.fragment.Fragment_RepairShop
 import luv.zoey.dotcom.fragment.menuFragmentAdapter
 
-class MenuActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     val toolbar by lazy { findViewById<Toolbar>(R.id.toolbar) }
     val fragmentAdapter by lazy { menuFragmentAdapter(supportFragmentManager) }
@@ -31,11 +35,10 @@ class MenuActivity : AppCompatActivity() {
         // [액션바] 초기화 단계
         setSupportActionBar(toolbar)
 
-        supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
-            setDisplayShowTitleEnabled(false)
+        toolbar_menu.setOnClickListener {
+            drawer_parent.openDrawer(Gravity.LEFT)
         }
+
         // [페이지뷰] 초기화 단계
         fragmentAdapter.apply {
             addItem(Fragment_Matching())
@@ -45,19 +48,33 @@ class MenuActivity : AppCompatActivity() {
 
         mainMenu_ViewPager.adapter = fragmentAdapter
         mainMenu_Indicator.setupWithViewPager(mainMenu_ViewPager, true)
+
+        navigationView.setNavigationItemSelectedListener(this)
+        navigationView.setOnClickListener {
+            Toast.makeText(this, "Clicked", Toast.LENGTH_LONG).show()
+        }
+
+
     }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            R.id.myEstimate_navMenu -> {
+                Toast.makeText(this, "${item.itemId} Clicked", Toast.LENGTH_LONG).show()
+                return true
+            }
 
-        var menuInflater = menuInflater
-        menuInflater.inflate(R.menu.toolbar_menu,menu)
+            R.id.myRepair_navMenu -> {
+                Toast.makeText(this, "${item.itemId} Clicked", Toast.LENGTH_LONG).show()
+                return true
+            }
 
-        return true
-    }
+            R.id.selfCheck_navMenu -> {
+                Toast.makeText(this, "${item.itemId} Clicked", Toast.LENGTH_LONG).show()
+                return true
+            }
+        }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        //TODO 나의 수리현황으로 이동
         return true
     }
 }
